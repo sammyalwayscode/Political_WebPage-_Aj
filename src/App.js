6b4660a73a3ce006621a86815c2d6d7e99229067
Olorunda_Samuel <olorundasamuel2@gmail.com>
2021-01-09 09:06:10 +0100

    A Political Website For Politicians in Ajegunle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Award from './Components/Awards/Award';
+import Recent from './Components/RecentWorks/Recent';
+import First from './Components/WelcomePage/First';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <First />
+      {/* <Recent /> */}
+      {/* <Award /> */}
     </div>
   );
 }
